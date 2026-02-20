fc59e4f86b3a730b4fca42c1cc53cbb5583927d5
Mike <vmichele80@hotmail.com>
2026-02-20 14:42:42 +0100

    Copied the code template from Codio

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-# This is the first python file of this project
+# Copied the code structure from Codio
 
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)

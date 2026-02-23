f65de53c37be18db6d41245c67940b5d311b7354
Mike <vmichele80@hotmail.com>
2026-02-23 10:48:07 +0100

    Added the code for the POST request on add route, created the additional function save posts, to rewrite the blog_post.json with the newly added post. might the intial json be empty or not existing, it will be created

--- app.py
+++ app.py
@@ -2,13 +2,39 @@
 
 import os
 import json
-from flask import Flask, current_app, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+
+def get_data_path():
+    # instance_path is a good place to store writable data files
+    return os.path.join(app.instance_path, "blog_posts.json")
+
+
+def save_posts(posts):
+    data_path = get_data_path()
+
+    # here I make sure the folder already exists in order to avoid crashes
+    # if the folder does exist, it will just overwrite the file, otherwise
+    # create a new one
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    with open(data_path, "w", encoding="utf-8") as f:
+        json.dump(posts, f, ensure_ascii=False, indent=2)
 
 def load_blog_posts():
-    data_path = os.path.join(current_app.instance_path, "blog_posts.json")
+    data_path = get_data_path()
 
-    with open(data_path, "r") as f:
-        return json.load(f)
+    # If file doesn't exist yet, return empty list
+    if not os.path.exists(data_path):
+        return []
+
+
+    with open(data_path, "r", encoding="utf-8") as f:
+        try:
+            return json.load(f)
+        except json.JSONDecodeError:
+            return []
+
+
 
 
 app = Flask(__name__)
@@ -21,8 +47,38 @@ def hello_world():
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        # We will fill this in the next step
-        pass
+        # Add the code that handles adding a new blog
+        author = request.form.get("author", "").strip()
+        title = request.form.get("title", "").strip()
+        content = request.form.get("content", "").strip()
+
+        # Think about validation of the entries
+
+        blog_posts = load_blog_posts()
+
+        # I generate the id. checking all the IDs in the json and looking
+        # for the Max value. in case no ID is present, setting it to zero
+        # does not affect the result and avoids the code from breaking
+        next_id = (max((p.get("id", 0) for p in blog_posts), default=0) + 1)
+
+        # I generate the new post entry to add to the json
+        new_post = {
+            "id": next_id,
+            "author": author,
+            "title": title,
+            "content": content
+        }
+
+        # I now add the new post entry to  the list of posts
+        # (generated from the json)
+        blog_posts.append(new_post)
+
+        # and now I save it in the original json
+        save_posts(blog_posts)
+        ...
+        return redirect(url_for('index'))
+
+    # this is what you get if the request is a GET
     return render_template('add.html')
 
 if __name__ == '__main__':

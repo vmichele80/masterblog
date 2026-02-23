a33a34216eb3e8cc18c03acbd9a45d04ffce32dc
Mike <vmichele80@hotmail.com>
2026-02-23 10:11:20 +0100

    Created add.html in order to get data for a new post from the user. Inserted the code for the post form, and integrated the necessary CSS in the style.css. in the app.py added the /add routing

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+# need to resume from - Adding the "Add" Route - chapter of Codio
 
 import os
 import json
-from flask import Flask, current_app
+from flask import Flask, current_app, request, render_template
 
 def load_blog_posts():
     data_path = os.path.join(current_app.instance_path, "blog_posts.json")
@@ -17,6 +18,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # We will fill this in the next step
+        pass
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
